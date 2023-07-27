e2eebf4a9ab5eeeb891b18abef03f29f732d62ee
teuliere <teuliere.eric@imsa.msa.fr>
2023-07-27 11:47:38 +0200

    Initial commit

---
+++ src/main/java/fr/padawan/jenkinsmvc/JenkinsmvcApplication.java
@@ -0,0 +1,13 @@
+package fr.padawan.jenkinsmvc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JenkinsmvcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JenkinsmvcApplication.class, args);
+	}
+
+}
---
+++ src/test/java/fr/padawan/jenkinsmvc/JenkinsmvcApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.padawan.jenkinsmvc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JenkinsmvcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
